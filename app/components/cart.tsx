78406c2173822799e73cd5d8bd78ca3d573a205d
Mohanned Kandil <mohanadmkandil@gmail.com>
2023-06-12 11:50:13 +0300

    style: add empty state in cart

--- app/components/cart.tsx
+++ app/components/cart.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useCartStore } from "@/store";
 import formatPrice from "@/utils/priceFormat";
 import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
+import basket from "@/public/basket.png";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -41,9 +42,22 @@ export default function Cart() {
             </div>
           </div>
         ))}
-        <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
-          Checkout
-        </button>
+
+        {cartStore.cart.length > 0 && (
+          <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
+            Checkout
+          </button>
+        )}
+
+        {!cartStore.cart.length && (
+          <div
+            className="flex flex-col items-center gap-12 text-2xl font-med
+           pt-56 opacity-75"
+          >
+            <h1>It's empty 😢</h1>
+            <Image src={basket} alt="empty cart" width={200} height={200} />
+          </div>
+        )}
       </div>
     </div>
   );
